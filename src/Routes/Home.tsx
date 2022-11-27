fa68ca6286d187fe98674149141efefb956c42ab
[jwhong70] <[jwhong70@gmail.com]>
2022-11-27 19:10:43 +0900

    #3-2 Home Layout

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,13 +1,84 @@
 import { useQuery } from "@tanstack/react-query";
 import { getMovies, IGetMoviesResult } from "../api";
+import styled from "styled-components";
+import { motion } from "framer-motion";
+import { useState } from "react";
+
+const Wrapper = styled.div`
+  background: black;
+  padding-bottom: 200px;
+`;
+const Loader = styled.div`
+  height: 20vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Banner = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 60px;
+  background-size: cover;
+`;
+const Slider = styled.div`
+  position: relative;
+  top: -100px;
+`;
+const Title = styled.h2`
+  font-size: 68px;
+  margin-bottom: 20px;
+`;
+const Overview = styled.p`
+  font-size: 30px;
+  width: 50%;
+`;
+const Row = styled(motion.div)`
+  display: grid;
+  gap: 5px;
+  grid-template-columns: repeat(6, 1fr);
+  position: absolute;
+  width: 100%;
+`;
+const Box = styled(motion.div)`
+  background-color: white;
+  background-size: cover;
+  background-position: center center;
+  height: 200px;
+  font-size: 66px;
+`;
+const offset = 6;
 
 function Home() {
   const { data, isLoading } = useQuery<IGetMoviesResult>(
     ["movies", "nowPlaying"],
     getMovies
   );
+  const [index, setIndex] = useState(0);
   return (
-    <div style={{ backgroundColor: "whitesmoke", height: "200vh" }}>home</div>
+    <Wrapper>
+      {isLoading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <>
+          <Banner>
+            <Title>{data?.results[0].title}</Title>
+            <Overview>{data?.results[0].overview}</Overview>
+          </Banner>
+          <Slider>
+            <Row>
+              {data?.results
+                .slice(1)
+                .slice(offset * index, offset * index + offset)
+                .map((movie) => (
+                  <Box key={movie.id} />
+                ))}
+            </Row>
+          </Slider>
+        </>
+      )}
+    </Wrapper>
   );
 }
 export default Home;
